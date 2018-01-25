5c5a94888cb1725e8f33f0fbb85be30094188d01
Leon <qinlanfeng@foxmail.com>
2018-01-26 01:02:59 +0800

    战旗主播爬取保存数据库

--- spider/zhanqi_anchor.py
+++ spider/zhanqi_anchor.py
@@ -43,7 +43,6 @@ class Handler(BaseHandler):
             cursor.execute('select cate_id,id from category where platform_id = %s;' % str(self.platform_id))
             results = cursor.fetchall()
             for item in results:
-                print(item)
                 self.crawl('https://apis.zhanqi.tv/static/v2.1/game/live/%s/%s/%s.json?ver=3.0.1&os=3&platform=4&publisher=8&4G=0' %
                            (item[0], str(self.limit), str(page),),
                            callback=self.index_page,
@@ -78,4 +77,106 @@ class Handler(BaseHandler):
         return {
             "url": response.url,
             "results": response.json['data'],
+            "cate_id": response.save['cate_id'],
+            "category_id": response.save['category_id'],
         }
+
+    def on_result(self,result):
+        if not result:
+            return
+        self.save_data(**result)
+        
+    def save_data(self, **kw):
+
+        if len(kw['results']) == 0:
+            return
+
+        if kw['results']:
+            item = kw['results']
+            try:
+                cursor = self.connect.cursor()
+                cursor.execute('select id from anchor where user_id=%s and platform_id=%s', (item['uid'], self.platform_id))
+                result = cursor.fetchone()
+                if result:
+                    sql = '''update anchor set 
+                        name=%s, 
+                        room_id=%s, 
+                        room_name=%s, 
+                        cover=%s,
+                        thumb_cover=%s,
+                        avatar=%s,
+                        avatar_mid=%s,
+                        avatar_small=%s,
+                        fans=%s,
+                        category_id=%s,
+                        cate_id=%s,
+                        online=%s,
+                        pc_url=%s,
+                        announcement=%s,
+                        update_time=%s,
+                        show_time=%s  
+                        where user_id=%s and platform_id=%s'''
+                    cursor.execute(sql, (item['nickname'],  
+                                         item['id'], 
+                                         item['title'], 
+                                         item['bpic'], 
+                                         item['spic'],
+                                         item['avatar'] + '-medium', 
+                                         item['avatar'] + '-medium', 
+                                         item['avatar'] + '-medium',  
+                                         item['follows'],  
+                                         kw['category_id'],  
+                                         kw['cate_id'],  
+                                         item['online'], 
+                                         'https://www.zhanqi.tv/' + item['code'],
+                                         ''.join(item['anchorNotice']),
+                                         datetime.now(), 
+                                         datetime.fromtimestamp(float(item['liveTime'])) if item['liveTime'] else datetime.now(),
+                                         item['uid'], 
+                                         self.platform_id))
+                else:
+                    # 插入操作
+                    sql = '''insert into anchor(
+                        user_id, 
+                        name, 
+                        room_id, 
+                        room_name, 
+                        cover, 
+                        thumb_cover,
+                        avatar, 
+                        avatar_mid, 
+                        avatar_small, 
+                        fans, 
+                        category_id, 
+                        cate_id, 
+                        online, 
+                        platform_id, 
+                        pc_url,
+                        announcement,
+                        show_time, 
+                        created_time) 
+                        values (%s, %s, %s, %s, %s, %s , %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'''
+                    cursor.execute(sql, (item['uid'], 
+                                         item['nickname'], 
+                                         item['id'], 
+                                         item['title'], 
+                                         item['bpic'],
+                                         item['spic'],
+                                         item['avatar'] + '-medium', 
+                                         item['avatar'] + '-medium', 
+                                         item['avatar'] + '-medium', 
+                                         item['follows'], 
+                                         kw['category_id'], 
+                                         kw['cate_id'], 
+                                         item['online'], 
+                                         self.platform_id,
+                                         'https://www.zhanqi.tv/' + item['code'],
+                                         ''.join(item['anchorNotice']),
+                                         datetime.fromtimestamp(float(item['liveTime'])) if item['liveTime'] else datetime.now(),
+                                         datetime.now(),
+                                        ))
+                self.connect.commit()
+
+            except Exception as e:
+                self.connect.rollback()
+                raise e
