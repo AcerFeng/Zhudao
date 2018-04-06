cf52b232d7b629a7657579acab07dc52f3299381
Leon <qinlanfeng@foxmail.com>
2018-04-06 18:16:12 +0800

    全部分类：动态调整item的宽度

--- template/js/list.js
+++ template/js/list.js
@@ -16,10 +16,44 @@ $(function() {
                 $('#main').addClass('main-move-right');
             }
         }
-
-
     });
-    
+
+    resetItemWidth();
 });
+
+
+function isHiddenSide() {
+    return $('#left-side').css('left') !== '0px';
+}
+
+$(window).resize(function() {
+    resetItemWidth();
+});
+
+function resetItemWidth() {
+    var screenWidth = $(window).width();
+    var mainWidth = screenWidth - (isHiddenSide() ? 0 : 240);
+    var itemWidth = '';
+    var margin = 20 + 30;
+    var num = 10;
+    if (screenWidth < 678) {
+        num = 3;
+        margin = 15 * 2;
+    } else if (screenWidth < 900) {
+        num = 5;
+    } else if (screenWidth < 1100) {
+        num = 6;
+    } else if (screenWidth < 1300) {
+        num = 7;
+    } else {
+        num = Math.floor(screenWidth / 200) + 1
+    }
+
+    itemWidth = Math.floor((mainWidth - margin - (num - 1) * 20 ) / num) + 'px';
+
+    var itemStyle = itemWidth ? '.category-list .item { width: ' + itemWidth + '!important}' : '';
+
+    $('#st').html('<style>' + itemStyle + '</style>')
+}
 
 
